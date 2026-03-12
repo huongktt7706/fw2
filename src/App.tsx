724df924678e4364140fcb50ceaa879093efa05b
huongktt7706 <thuyhuong7706nb@gmail.com>
2026-03-12 15:03:32 +0700

    tra no thay Hoa

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,137 @@
 import { Toaster } from "react-hot-toast";
 import { Link } from "react-router-dom";
+import { Table } from "antd";
 
 function App() {
+  const columns = [
+    { title: "ID", dataIndex: "key" },
+    { title: "Name", dataIndex: "name" },
+    { title: "Age", dataIndex: "age" },
+    { title: "Major", dataIndex: "major" },
+  ];
+
+  const data = [
+    { key: 1, name: "John", age: 25, major: "Dev" },
+    { key: 2, name: "Anna", age: 30, major: "Dev" },
+    { key: 3, name: "Anna", age: 30, major: "Dev" },
+    { key: 4, name: "Anna", age: 30, major: "Dev" },
+    { key: 5, name: "Anna", age: 30, major: "Dev" },
+
+  ];
+  const productColumns = [
+    {
+      title: "ID",
+      dataIndex: "id",
+    },
+    {
+      title: "Product Name",
+      dataIndex: "name",
+    },
+    {
+      title: "Price",
+      dataIndex: "price",
+    },
+    {
+      title: "Category",
+      dataIndex: "category",
+    },
+  ];
+  const productData = [
+    {
+      key: 1,
+      id: 1,
+      name: "Laptop",
+      price: 1200,
+      category: "Electronics",
+    },
+    {
+      key: 2,
+      id: 2,
+      name: "Phone",
+      price: 800,
+      category: "Electronics",
+    },
+    {
+      key: 3,
+      id: 3,
+      name: "Shoes",
+      price: "$100",
+      category: "Fashion",
+    },
+    {
+      key: 4,
+      id: 4,
+      name: "Watch",
+      price: 200,
+      category: "Accessories",
+    },
+    {
+      key: 5,
+      id: 5,
+      name: "Backpack",
+      price: 50,
+      category: "Fashion",
+    },
+  ];
+  const userColumns = [
+  {
+    title: "ID",
+    dataIndex: "id",
+  },
+  {
+    title: "Name",
+    dataIndex: "name",
+  },
+  {
+    title: "Email",
+    dataIndex: "email",
+  },
+  {
+    title: "Status",
+    dataIndex: "status",
+    render: (status:string) => (
+      <span className={status === "active" ? "text-green-500" : "text-red-500"}>
+        {status}
+      </span>
+    ),
+  },
+  // {
+  //   title: "Action",
+  //   render: () => (
+  //     <>
+  //       <button className="bg-yellow-400 px-3 py-1 mr-2 rounded">
+  //         Edit
+  //       </button>
+  //       <button className="bg-red-500 text-white px-3 py-1 rounded">
+  //         Delete
+  //       </button>
+  //     </>
+  //   ),
+  // },
+];
+const userData = [
+  {
+    key: 1,
+    id: 1,
+    name: "h",
+    email: "h@gmail.com",
+    status: "active",
+  },
+  {
+    key: 2,
+    id: 2,
+    name: "A",
+    email: "a@gmail.com",
+    status: "inactive",
+  },
+  {
+    key: 3,
+    id: 3,
+    name: "D",
+    email: "d@gmail.com",
+    status: "active",
+  },
+];
   return (
     <>
       <nav className="bg-blue-600 text-white shadow">
@@ -30,12 +160,17 @@ function App() {
               Đăng ký
             </Link>
           </div>
+
         </div>
       </nav>
 
       {/* MAIN CONTENT */}
       <div className="max-w-6xl mx-auto mt-10 px-4 text-center">
         <h1 className="text-4xl font-bold mb-4">Chào mừng đến với WEB2091</h1>
+        <Table columns={columns} dataSource={data} pagination={{ pageSize: 2 }} />
+        <h1>Product</h1>
+        <Table columns={productColumns} dataSource={productData} pagination={{ pageSize: 2 }} />
+        <Table columns={userColumns} dataSource={userData} pagination={{ pageSize: 2 }} />
       </div>
 
       <Toaster />
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
@@ -8,6 +8,9 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <Routes>
+        <Route path="" element=""/>
+      </Routes>
     </BrowserRouter>
   </StrictMode>
 );
